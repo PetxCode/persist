3a95e04cfd93e211940a9370991a91131c07f8cf
PetxCode <brighterdayscodelab@gmail.com>
2024-07-23 18:19:13 +0100

    adding content

---
+++ app/content/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to our website!</h1>
+        <p>This is the default page.</p>
+      </div>
+    </main>
+  );
+};
+
+export default page;
--- app/global/slice.tsx
+++ app/global/slice.tsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   mode: false,
+  userName: "",
 };
 
 const slice = createSlice({
@@ -11,9 +12,12 @@ const slice = createSlice({
     toggleMode: (state) => {
       state.mode = !state.mode;
     },
+    changeUserName: (state: { userName: string }, { payload }) => {
+      state.userName = payload;
+    },
   },
 });
 
-export const { toggleMode } = slice.actions;
+export const { toggleMode, changeUserName } = slice.actions;
 
 export default slice.reducer;
