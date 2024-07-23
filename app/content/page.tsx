e880f301cf08b4e1c48f43d455fc1107967957fd
PetxCode <brighterdayscodelab@gmail.com>
2024-07-23 19:08:51 +0100

    input for name intake

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
 const page = () => {
+  const mainAction = () => {
+    // e.preventDefault();
+    // console.log(e.target);
+  };
+
   return (
     <main>
       <div>
         <h1>Welcome to our website!</h1>
         <p>This is the default page.</p>
+
+        <div className="mt-10 w-[400px]">
+          <div className="">
+            <div className="flex flex-col">
+              <label className="font-semibold text-[12px] mb-2">
+                Enter your Name
+              </label>
+
+              <input
+                type="text"
+                name="name"
+                className="border rounded-md h-[45px] outline-none pl-2 "
+              />
+            </div>
+
+            <button
+              onClick={mainAction}
+              type="submit"
+              className="mt-5 border flex justify-center items-center h-[50px] w-full bg-neutral-900 text-white rounded-md"
+            >
+              Created
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
